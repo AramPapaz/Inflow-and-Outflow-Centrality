738dc92bc86d3445c4272335d2cc4c7fb207fe23
Alexander Gress <alexander.gress@helmholtz-hips.de>
2026-07-07 12:47:50 +0200

    Used claude to transform the project into a pip package
    Also added an option to calculate inflow and outflow at the same time

---
+++ src/inflow_outflow_centrality/__init__.py
@@ -0,0 +1,11 @@
+from .utilities import (
+    flow_metric,
+    node_weighted_degree_centrality,
+    node_weighted_closeness_centrality,
+)
+
+__all__ = [
+    "flow_metric",
+    "node_weighted_degree_centrality",
+    "node_weighted_closeness_centrality",
+]
